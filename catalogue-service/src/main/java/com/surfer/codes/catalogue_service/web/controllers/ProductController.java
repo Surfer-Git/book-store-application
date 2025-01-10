3244413c60d7df2aecfea401afbf69110172cd39
priyanshu <priyanshupriyadarshi555@gmail.com>
2025-01-10 20:16:09 +0000

    Add: lombok annotation processor

--- catalogue-service/src/main/java/com/surfer/codes/catalogue_service/web/controllers/ProductController.java
+++ catalogue-service/src/main/java/com/surfer/codes/catalogue_service/web/controllers/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/products")
 class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
