edd36be63cb1abc4369f4414866c7db29ca2a98d
priyanshu <priyanshupriyadarshi555@gmail.com>
2025-01-15 18:15:09 +0000

    Add: order-db flyway migrations, order-service github workflow

---
+++ order-service/src/main/resources/db/migration/V2__create_order_item_table.sql
@@ -0,0 +1,12 @@
+create sequence order_item_id_seq start with 1 increment by 50;
+
+create table order_items
+(
+    id       bigint default nextval('order_item_id_seq') not null,
+    code     text not null,
+    name     text not null,
+    price    numeric not null,
+    quantity integer not null,
+    primary key (id),
+    order_id bigint  not null references orders (id)
+);
