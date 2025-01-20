d40ddf59a32fd433a81363b9a04a0f4ea29edec9
priyanshu <priyanshupriyadarshi555@gmail.com>
2025-01-20 19:25:18 +0000

    Add: order-service : global exception handlers

---
+++ order-service/src/main/java/com/surfer/codes/order_service/web/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.surfer.codes.order_service.web.exception;
+
+import java.net.URI;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.*;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final URI NOT_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/not-found");
+    private static final URI ISE_TYPE = URI.create("https://api.bookstore.com/errors/server-error");
+    private static final URI BAD_REQUEST_TYPE = URI.create("https://api.bookstore.com/errors/bad-request");
+    private static final String SERVICE_NAME = "order-service";
+
+    @ExceptionHandler(Exception.class)
+    ProblemDetail handleUnhandledException(Exception e) {
+        ProblemDetail problemDetail =
+                ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setType(ISE_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    /**
+     * thrown when request-dto is invalid. hence, controller method arguments invalid.
+     */
+    @Override
+    @Nullable protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            errors.add(error.getDefaultMessage());
+        });
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Invalid Request");
+        problemDetail.setTitle("Bad Request");
+        problemDetail.setType(BAD_REQUEST_TYPE);
+        problemDetail.setProperty("errors", errors);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("timestamp", Instant.now());
+        return ResponseEntity.badRequest().body(problemDetail);
+    }
+}
