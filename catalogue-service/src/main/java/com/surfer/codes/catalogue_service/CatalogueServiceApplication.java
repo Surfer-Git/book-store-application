e6c4c95358b24507f3c7529bf05ec37edb12fa44
Surfer-Git <priyanshupriyadarshi555@gmail.com>
2025-03-14 22:50:06 +0530

    1. Add api-gateway 2. Add swagger ui 3. Add github workflows for notification-service, api_gateway 4. Dockerize the api_gateway

--- catalogue-service/src/main/java/com/surfer/codes/catalogue_service/CatalogueServiceApplication.java
+++ catalogue-service/src/main/java/com/surfer/codes/catalogue_service/CatalogueServiceApplication.java
@@ -3,6 +3,9 @@ package com.surfer.codes.catalogue_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -10,5 +13,19 @@ public class CatalogueServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CatalogueServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedMethods("*")
+                        .allowedOriginPatterns("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
     }
 }
--- order-service/src/main/java/com/surfer/codes/order_service/OrderServiceApplication.java
+++ order-service/src/main/java/com/surfer/codes/order_service/OrderServiceApplication.java
@@ -4,7 +4,10 @@ import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -14,5 +17,19 @@ public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedMethods("*")
+                        .allowedOriginPatterns("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
     }
 }
