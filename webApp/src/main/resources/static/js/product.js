9756d8872b80017f4c8d532c1807e9f5ec708f05
Surfer-Git <priyanshupriyadarshi555@gmail.com>
2025-04-01 01:08:10 +0530

    WIP: fix alpine.js loading issue

--- webApp/src/main/resources/static/js/product.js
+++ webApp/src/main/resources/static/js/product.js
@@ -1,15 +1,10 @@
-console.log('product.js loaded');
-try {
-    document.addEventListener('alpine:init', () => {
-        console.log('alpine:init fired');
+console.log("Inside Product.js");
+document.addEventListener('alpine:init', () => {
         Alpine.data('productInit', () => ({
             count: 0,
             increase() {
                 this.count++;
-                console.log('count increased:', this.count);
+                console.log('count: ', this.count);
             }
-        }))
-    });
-} catch (error) {
-    console.error('Error in Alpine.js initialization:', error);
-}
+        }));
+    })
