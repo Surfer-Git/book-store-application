c4367c3fd36564c789c77709ffacceabd1d0058c
priyanshu <priyanshupriyadarshi555@gmail.com>
2025-05-12 19:26:17 +0000

    fix: docker image issue

--- webApp/src/main/java/com/surfer/codes/webApp/clients/orders/CreateOrderRequest.java
+++ webApp/src/main/java/com/surfer/codes/webApp/clients/orders/CreateOrderRequest.java
@@ -1,12 +1,12 @@
 package com.surfer.codes.webApp.clients.orders;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import java.io.Serializable;
 import java.util.Set;
 
 public record CreateOrderRequest(
-        @NotBlank(message = "Items cannot be empty.") Set<OrderItem> items,
+        @NotEmpty(message = "Items cannot be empty.") Set<OrderItem> items,
         @Valid Customer customer,
         @Valid Address deliveryAddress)
         implements Serializable {}
